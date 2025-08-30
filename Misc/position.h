879763529701c86e06c92b4c5fedc48148208ee9
TalhaProgrammer92 <talha.code92@gmail.com>
2025-08-30 19:14:29 +0500

    Added position class to handle positions of different pieces

---
+++ Misc/position.h
@@ -0,0 +1,37 @@
+#ifndef POSITION_H
+#define POSITION_H
+
+namespace Misc
+{
+    class Position
+    {
+        // * Attributes
+        int row, column;
+        
+    public:
+        // * Constructor
+        Position();
+        Position(int row, int column);
+
+        // * Getters
+        int get_row() const;
+        int get_column() const;
+
+        // * Setters
+        void set_row(const int &row);
+        void set_column(const int &column);
+
+        // * Methods - Comparison
+        bool operator==(const Position &other) const;
+        bool operator!=(const Position &other) const;
+
+        // * Methods - Arithmetic
+        Position operator+(const Position &other) const;
+        Position operator-(const Position &other) const;
+
+        // * Method - Get Absolute Difference
+        Position abs_diff(const Position &position2) const;
+    };
+}
+
+#endif
---
+++ Misc/postion.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "position.h"
+
+using namespace std;
+using namespace Misc;
+
+// * Constructor
+Position::Position() : row(0), column(0) {}
+Position::Position(int row, int column) : row(row), column(column) {}
+
+// * Getters
+int Position::get_row() const
+{
+    return row;
+}
+
+int Position::get_column() const
+{
+    return column;
+}
+
+// * Setters
+void Position::set_row(const int &row)
+{
+    if (row >= 0 && row < 8)
+        this->row = row;
+}
+
+void Position::set_column(const int &column)
+{
+    if (column >= 0 && column < 8)
+        this->column = column;
+}
+
+// * Methods - Comparison
+bool Position::operator==(const Position &other) const
+{
+    return (row == other.row && column == other.column);
+}
+
+bool Position::operator!=(const Position &other) const
+{
+    return !(*this == other);
+}
+
+// * Methods - Arithmetic
+Position Position::operator+(const Position &other) const
+{
+    return Position(row + other.row, column + other.column);
+}
+
+Position Position::operator-(const Position &other) const
+{
+    return Position(row - other.row, column - other.column);
+}
+
+// * Method - Get Absolute Difference
+Position Position::abs_diff(const Position &position2) const
+{
+    return Position(abs(row - position2.row), abs(column - position2.column));
+}
